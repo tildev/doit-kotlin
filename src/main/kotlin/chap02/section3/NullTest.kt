4c9daf604026ae7ed87d60002426b927e7ff2474
이은지 <tildev@ieunjiui-MacBookAir.local>
2024-10-27 17:06:29 +0900

    feat: Add NullTest code

---
+++ src/main/kotlin/chap02/section3/NullTest.kt
@@ -0,0 +1,22 @@
+package chap02.section3
+
+fun main() {
+    var str1: String = "Hello Kotlin"
+    // 오류. null 을 허용하지 않음
+    // str1 = null
+    println("str1: $str1")
+
+    // null 할당을 허용하려면 자료형 뒤에 물음표(?) 기호를 명시해야 함.
+    var str2: String? = "Hello Kotlin"
+    str2 = null
+    println("str2: $str2")
+    // .length 불가능. null 을 허용하면 length 가 실행될 수 없음.
+    //println("str2: $str2 length: ${str2.length}")
+    // 세이프 콜 로 안전하게 호출 가능.
+    println("str2: $str2 length: ${str2?.length}")
+    // not null 단정기호 !! 를 사용하여 NPE 발생시킬 수 있음.
+    // println("str2: $str2 length: ${str2!!}")
+
+    val len = if(str2 !=null ) str2.length else -1
+    println("str2: $str2 length: ${len}")
+}
