ac326525bbaf6255bf55135b557105fdef956057
tildev <tildevelove@gmail.com>
2025-01-19 21:42:52 +0900

    feat: Add chap07_section2 inner class

--- src/main/kotlin/chap07/section2/InnerClassTest.kt
+++ src/main/kotlin/chap07/section2/InnerClassTest.kt
@@ -6,9 +6,22 @@ class SmartPhone(val model: String) {
     inner class ExternalStorage(val size: Int) {
         fun getInfo() = "${model}: Installed on $cpu with ${size}Gb" // 바깥 클래스의 프로퍼티 접근
     }
+
+    fun powerOn(): String {
+        class Led(val color: String) { // 지역 클래스 선언
+            fun blink(): String = "Blinking $color on $model" // 외부의 프로퍼티는 접근 가능
+        }
+
+        val powerStatus = Led("Red") // 여기에서 지역 클래스가 사용됨
+        return powerStatus.blink()
+    } // powerOn() 블록 끝
 }
 
 fun main() {
     val mySdcard = SmartPhone("S7").ExternalStorage(32)
     println(mySdcard.getInfo())
+
+    val myPhone = SmartPhone("Note9")
+    myPhone.ExternalStorage(128)
+    println(myPhone.powerOn())
 }
