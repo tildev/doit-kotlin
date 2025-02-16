4549af324bbf4509f6bf2ee89845453aa8be9031
tildev <tildevelove@gmail.com>
2025-02-16 12:02:59 +0900

    feat: Add chap08_section1 - LambdaParameter

--- src/main/kotlin/chap08/section1/LambdaParameter.kt
+++ src/main/kotlin/chap08/section1/LambdaParameter.kt
@@ -11,6 +11,12 @@ fun <T> add(a: T, b: T, op: (T, T) -> T): T {
     return op(a, b)
 }
 
+typealias arithmetic<T> = (T, T) -> T
+
+fun <T> addAux(a: T, b: T, op: arithmetic<T>): T {
+    return op(a, b)
+}
+
 fun main() {
     // 함수가 실행될 때 넘겨지는 인자이므로 연산식을 함수 선언부에 직접 구현하지 않고 전달하는 방법을 사용.
     // 함수의 형식 매개변수로 자료형을 특정하지 않아도 실행이 가능
