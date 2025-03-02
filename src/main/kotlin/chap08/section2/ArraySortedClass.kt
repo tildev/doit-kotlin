0aa93885dc7de7bbb92a7e7f38b8f127845cb8f7
tildev <tildevelove@gmail.com>
2025-03-02 16:28:56 +0900

    feat: Add chap08_section2 - AarrySortedClass

---
+++ src/main/kotlin/chap08/section2/ArraySortedClass.kt
@@ -0,0 +1,17 @@
+package chap08.section2
+
+data class Product(val nane: String, val price: Double)
+
+fun main() {
+    val products = arrayOf(
+        Product("Snow Ball", 870.00),
+        Product("Smart Phone", 990.00),
+        Product("Drone", 240.00),
+        Product("Mouse", 333.55),
+        Product("Keyboard", 125.99),
+        Product("Monitor", 1500.99),
+        Product("Tablet", 512.99),
+    )
+    products.sortBy { it.price } // 값에 따라 정렬
+    products.forEach { println(it) }
+}
