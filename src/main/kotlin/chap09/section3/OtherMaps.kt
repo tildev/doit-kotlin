781a929829706b4d610a078161fa4ccae563ae26
tildev <tildevelove@gmail.com>
2025-03-03 18:14:04 +0900

    feat: Add chap09_section2 - OtherMap

---
+++ src/main/kotlin/chap09/section3/OtherMaps.kt
@@ -0,0 +1,14 @@
+package chap09.section3
+
+import java.util.*
+
+fun main() {
+    val hashMap: HashMap<Int, String> = hashMapOf(1 to "Hello", 2 to "World")
+    println("hashMap = $hashMap")
+
+    val sortedMap: SortedMap<Int, String> = sortedMapOf(1 to "Apple", 2 to "Banana")
+    println("sortedMap = $sortedMap")
+
+    val linkedHash: LinkedHashMap<Int, String> = linkedMapOf(1 to "Computer", 2 to "Mouse")
+    println("linkedHash = $linkedHash")
+}
