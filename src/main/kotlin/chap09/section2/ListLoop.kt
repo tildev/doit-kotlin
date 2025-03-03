a3d15c35e8495f1bd0ac3b4559043773d60cc8ac
tildev <tildevelove@gmail.com>
2025-03-03 17:27:06 +0900

    feat: Add chap09_section2 - ListLoop

--- src/main/kotlin/chap09/section2/ListLoop.kt
+++ src/main/kotlin/chap09/section2/ListLoop.kt
@@ -5,4 +5,20 @@ fun main() {
     for (item in fruits) {
         println(item)
     }
+
+    for (index in fruits.indices) {
+        println("fruits[$index] = ${fruits[index]}")
+    }
+    for (index in fruits.indices) {
+        if (index % 2 == 0) println("fruits[$index] = ${fruits[index]}")
+    }
+
+    var index = 0
+    while (index < fruits.size) {
+        println("fruits[$index] = ${fruits[index]}")
+        index++
+    }
+    val emptyList: List<String> = emptyList<String>()
+    val nonNullsList: List<Int> = listOfNotNull(2, 45, 2, null, 5, null)
+    println(nonNullsList)
 }
