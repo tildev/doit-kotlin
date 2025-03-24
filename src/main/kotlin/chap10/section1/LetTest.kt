f1aacd2b09fb250ee135cc51670a06dbb2c7048b
tildev <tildevelove@gmail.com>
2025-03-24 13:50:41 +0900

    feat: Add chap10_section1 - LetTest

---
+++ src/main/kotlin/chap10/section1/LetTest.kt
@@ -0,0 +1,20 @@
+package chap10.section1
+
+fun main() {
+    val score: Int? = 32
+    //var score = null
+
+    fun checkScore() {
+        if (score != null) {
+            println("Score: $score")
+        }
+    }
+
+    fun checkScoreLet() {
+        score?.let { println("Score: $it") }
+        val str = score.let { it.toString() }
+        println(str)
+    }
+    checkScore()
+    checkScoreLet()
+}
